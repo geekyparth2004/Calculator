dc97b2cfaa3beef9a004139d2f3c0f1b8fb95dda
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-01-22 20:14:06 +0530

    w

---
+++ To Do List/index.js
@@ -0,0 +1,39 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("Please add a Task!!");
+    }
+    else{
+        let li= document.createElement('li');
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement('span');
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = '';
+    save_data();
+}
+
+listContainer.addEventListener("click",function(e){
+    if (e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        save_data();
+    }
+    else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        save_data();
+    }
+},false);
+
+function save_data(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+
+function show_list(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+show_list();
